385817f73493d433d762fe202cfdba7f3c25ec62
XYShaoKang <38753204+XYShaoKang@users.noreply.github.com>
2021-11-08 00:50:40 +0800

    chore: fix base url

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ const { resolve } = require('path')
 const { defineConfig } = require('vite')
 
 module.exports = defineConfig({
+  base: '/dom-xss-demo/',
   build: {
     rollupOptions: {
       input: {
